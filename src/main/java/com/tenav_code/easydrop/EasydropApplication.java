6b450c58f2dd543da65bf5a35adcb41ffdc0f0a5
Chongda You <74991409+chongdayou@users.noreply.github.com>
2025-07-01 21:33:25 -0500

    Initial commit to repository.

---
+++ src/main/java/com/tenav_code/easydrop/EasydropApplication.java
@@ -0,0 +1,13 @@
+package com.tenav_code.easydrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasydropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasydropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenav_code/easydrop/EasydropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenav_code.easydrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasydropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
